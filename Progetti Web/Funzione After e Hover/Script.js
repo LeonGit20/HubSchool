203ae257657bcdc342744764b96a1d009ac8c0bf
LeonGit20 <francesco.ferrante2000@gmail.com>
2025-07-18 10:34:47 +0200

    modifica

--- Progetti Web/Funzione After e Hover/Script.js
+++
@@ -1,3 +0,0 @@
-function Shinichi_Kudo() {document.getElementById("myImage").src = "Img/Infobox_-_Shinichi_Kudo.png";}
-function Conan_Edogawa() {document.getElementById("myImage").src = "Img/Conan_Edogawa.jpg";}
-function Name() {document.getElementById("Paragrafo").innerHTML = "Conan Edogawa";}
